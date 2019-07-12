1c8f752ad3313e23d088d5632b2c7a40a0fcee8a
marc wang <marc.wang@uwaterloo.ca>
2019-07-12 14:34:13 -0400

    reformatted dcgan

--- dcgan.py
+++ dcgan.py
@@ -3,11 +3,12 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 
+
 def make_discriminator_model(isize, nc, ndf, n_extra_layers=0):
     assert isize % 16 == 0, "isize has to be a multiple of 16"
 
     model = nn.Sequential(
-        nn.Conv3d(nc,ndf,4,2, padding = 1, bias=False),
+        nn.Conv3d(nc, ndf, 4, 2, padding=1, bias=False),
         nn.LeakyReLU(0.2, inplace=True)
     )
 
@@ -15,29 +16,29 @@ def make_discriminator_model(isize, nc, ndf, n_extra_layers=0):
 
     for _ in range(n_extra_layers):
         model.add_module(str(i),
-                        nn.Conv3d(cndf, cndf, 3, 1, 1, bias=False))
+                         nn.Conv3d(cndf, cndf, 3, 1, 1, bias=False))
         model.add_module(str(i+1),
-                        nn.BatchNorm3d(cndf))
+                         nn.BatchNorm3d(cndf))
         model.add_module(str(i+2),
-                        nn.LeakyReLU(0.2, inplace=True))
+                         nn.LeakyReLU(0.2, inplace=True))
         i += 3
 
     while csize > 4:
         in_feat = cndf
         out_feat = cndf * 2
         model.add_module(str(i),
-                        nn.Conv3d(in_feat, out_feat, 4, 2, 1, bias=False))
+                         nn.Conv3d(in_feat, out_feat, 4, 2, 1, bias=False))
         model.add_module(str(i+1),
-                        nn.BatchNorm3d(out_feat))
+                         nn.BatchNorm3d(out_feat))
         model.add_module(str(i+2),
-                        nn.LeakyReLU(0.2, inplace=True))
-        i+=3
+                         nn.LeakyReLU(0.2, inplace=True))
+        i += 3
         cndf = cndf * 2
         csize = csize / 2
 
     # state size. K x 4 x 4 x 4
     model.add_module(str(i),
-                    nn.Conv3d(cndf, 1, 4, 1, 0, bias=False))
+                     nn.Conv3d(cndf, 1, 4, 1, 0, bias=False))
     model.add_module(str(i+1), nn.Sigmoid())
 
     return model
@@ -61,11 +62,11 @@ def make_generator_model(isize, nz, nc, ngf, n_extra_layers=0):
     i, csize = 3, 4
     while csize < isize//2:
         model.add_module(str(i),
-            nn.ConvTranspose3d(cngf, cngf//2, 4, 2, 1, bias=False))
+                         nn.ConvTranspose3d(cngf, cngf//2, 4, 2, 1, bias=False))
         model.add_module(str(i+1),
-                        nn.BatchNorm3d(cngf//2))
+                         nn.BatchNorm3d(cngf//2))
         model.add_module(str(i+2),
-                        nn.ReLU(True))
+                         nn.ReLU(True))
         i += 3
         cngf = cngf // 2
         csize = csize * 2
@@ -73,15 +74,15 @@ def make_generator_model(isize, nz, nc, ngf, n_extra_layers=0):
     # Extra layers
     for _ in range(n_extra_layers):
         model.add_module(str(i),
-                        nn.Conv3d(cngf, cngf, 3, 1, 1, bias=False))
+                         nn.Conv3d(cngf, cngf, 3, 1, 1, bias=False))
         model.add_module(str(i+1),
-                        nn.BatchNorm3d(cngf))
+                         nn.BatchNorm3d(cngf))
         model.add_module(str(i+2),
-                        nn.ReLU(True))
+                         nn.ReLU(True))
         i += 3
 
     model.add_module(str(i),
-                    nn.ConvTranspose3d(cngf, nc, 4, 2, 1, bias=False))
+                     nn.ConvTranspose3d(cngf, nc, 4, 2, 1, bias=False))
     model.add_module(str(i+1), nn.Tanh())
 
     return model
