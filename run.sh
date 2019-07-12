d0763795353afba729b5740208335ab5b61fb44e
marc wang <marc.wang@uwaterloo.ca>
2019-07-12 13:40:29 -0400

    removed bash file

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-####################################
-#
-# tensorflow 
-#
-####################################
-
-docker run --runtime=nvidia -it -p 8888:8888 -v $PWD:/tmp -w /tmp tensorflow/tensorflow:1.12.3-gpu-py3
--- run_bash.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-####################################
-#
-# tensorflow 
-#
-####################################
-
-docker run --runtime=nvidia -it -v $PWD:/tmp -w /tmp tensorflow/tensorflow:1.12.3-gpu-py3 bash
