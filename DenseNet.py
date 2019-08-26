c7866baaf8334ecfd8fe17a85be35b5f0149b5fd
marc wang <marc.wang@uwaterloo.ca>
2019-08-26 14:11:44 -0400

    added densenet code

--- DenseNet.py
+++ DenseNet.py
@@ -7,7 +7,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-
 class BasicBlock(nn.Module):
     def __init__(self, in_planes, out_planes, dropRate=0.0):
         super(BasicBlock, self).__init__()
@@ -69,10 +68,10 @@ class DenseBlock(nn.Module):
     def forward(self, x):
         return self.layer(x)
 
-class DenseNet3(nn.Module):
+class Generator(nn.Module):
     def __init__(self, depth, num_classes, growth_rate=12,
                  reduction=0.5, bottleneck=True, dropRate=0.0):
-        super(DenseNet3, self).__init__()
+        super(Generator, self).__init__()
         in_planes = 2 * growth_rate
         n = (depth - 4) / 3
         if bottleneck == True:
@@ -82,7 +81,7 @@ class DenseNet3(nn.Module):
             block = BasicBlock
         n = int(n)
         # 1st conv before any dense block
-        self.conv1 = nn.Conv3d(3, in_planes, kernel_size=3, stride=1,
+        self.conv1 = nn.Conv3d(1, in_planes, kernel_size=16, stride=1,
                                padding=1, bias=False)
         # 1st block
         self.block1 = DenseBlock(n, in_planes, growth_rate, block, dropRate)
@@ -96,28 +95,67 @@ class DenseNet3(nn.Module):
         in_planes = int(math.floor(in_planes*reduction))
         # 3rd block
         self.block3 = DenseBlock(n, in_planes, growth_rate, block, dropRate)
+        self.trans3 = TransitionBlock(in_planes, int(math.floor(in_planes*reduction)), dropRate=dropRate)
         in_planes = int(in_planes+n*growth_rate)
-        # global average pooling and classifier
-        self.bn1 = nn.BatchNorm3d(in_planes)
-        self.relu = nn.ReLU(inplace=True)
-        self.fc = nn.Linear(in_planes, num_classes)
-        self.in_planes = in_planes
+        
+        # 4th block
+        self.block4 = DenseBlock(n, in_planes, growth_rate, block, dropRate)
 
-        for m in self.modules():
-            if isinstance(m, nn.Conv3d):
-                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, math.sqrt(2. / n))
-            elif isinstance(m, nn.BatchNorm3d):
-                m.weight.data.fill_(1)
-                m.bias.data.zero_()
-            elif isinstance(m, nn.Linear):
-                m.bias.data.zero_()
+        self.recon = nn.Conv3d(in_planes, 1, kernel_size=1, stride=1, padding=0, bias=False)
+
     def forward(self, x):
         out = self.conv1(x)
-        out = self.trans1(self.block1(out))
-        out = self.trans2(self.block2(out))
+        out = self.block1(torch.cat([x, out], 1))
+        out = self.trans1(out)
+        out = self.block2(out)
+        out = self.trans2(out)
         out = self.block3(out)
-        out = self.relu(self.bn1(out))
-        out = F.avg_pool3d(out, 8)
-        out = out.view(-1, self.in_planes)
-        return self.fc(out)
+        out = self.trans3(out)
+        out = self.block4(out)
+        return self.recon(out)
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+
+        self.net = nn.Sequential(
+            nn.Conv3d(3, 64, kernel_size=3, padding=1),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv3d(64, 64, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm3d(64),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv3d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm3d(128),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv3d(128, 128, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm3d(128),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv3d(128, 256, kernel_size=3, padding=1),
+            nn.BatchNorm3d(256),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv3d(256, 256, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm3d(256),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv3d(256, 512, kernel_size=3, padding=1),
+            nn.BatchNorm3d(512),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv3d(512, 512, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm3d(512),
+            nn.LeakyReLU(0.2),
+
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv3d(512, 1024, kernel_size=1),
+            nn.LeakyReLU(0.2),
+            nn.Conv3d(1024, 1, kernel_size=1)
+        )
+
+    def forward(self, x):
+        batch_size = x.size(0)
+        return F.sigmoid(self.net(x).view(batch_size))
